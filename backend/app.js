8c274023b2c266192a452b7190720d4d5cb6a696
Indresh kumawat <indreshkmwt15@gmail.com>
2024-04-05 23:50:58 +0530

    url

--- backend/app.js
+++ backend/app.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(
 	cors({
 		origin: [
-			"http://localhost:3000","https://lifeline-1.onrender.com",
+			"https://lifeline-1.onrender.com","http://localhost:3000",
 		],
 		credentials: true,
 	})
